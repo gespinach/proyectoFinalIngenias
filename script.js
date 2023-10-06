053262abc68eeaf554b4ba2221e25a54dff782e2
Barbfh <barbarah996@gmail.com>
2023-10-05 21:00:59 -0300

    login
    Se agrega login y registro

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function login() {
-    let nombre = prompt("Ingrese su nombre de Usuario")
-    let clave = prompt("Ingrese su clave")
-
-    if (nombre == "" || clave == "" ) {
-        alert("Por favor, complete los campos requeridos")
-    } else {
-        alert("Usuario logueado con exito")
-    }
-}
-
-
 let boton = document.querySelector("#botonDetalleLibroDelDia");
 var divDetalle = document.getElementById("divDetalleLibroDelDia");
 var boton1 = document.getElementById("botonDetalleLibroDelDia");
